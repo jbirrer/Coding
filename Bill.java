4dfd44987524a57f22533b71b345d37afc637a2e
jbirrer <birrer.joel@hotmail.ch>
2020-12-06 23:05:08 +0100

    l

--- Bill.java
+++ Bill.java
@@ -1,10 +1,6 @@
 public class Bill{
     public static void main(String[] args) {
         bigBill game = new bigBill("Lenzerheide");
-        Player joel = new Player("joel", game);
-        Player andri = new Player("andri", game);
-        Player julia = new Player("julia", game);
-        Player meli = new Player("meli", game);
         if(game.totalcost != 0)
         game.calculateForAll();
     }
